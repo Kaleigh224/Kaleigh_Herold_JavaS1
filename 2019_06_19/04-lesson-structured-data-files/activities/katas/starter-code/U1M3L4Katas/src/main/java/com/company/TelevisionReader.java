b4de0b8b9994b6e6f095a88f2bd6e7f758cbb353
Kaleigh Herold <kaleigh224@gmail.com>
2019-06-18 21:04:03 -0400

    adding L3 & L4 katas

---
+++ 2019_06_19/04-lesson-structured-data-files/activities/katas/starter-code/U1M3L4Katas/src/main/java/com/company/TelevisionReader.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.type.TypeReference;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public class TelevisionReader {
+    public static void main(String[] args) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            List<Television> tvList;
+
+            tvList = mapper.readValue(new File("television.json"), new TypeReference<List<Television>>(){});
+
+            for(Television tv : tvList) {
+                System.out.println("========================");
+                System.out.println(tv.getBrand());
+                System.out.println(tv.getModel());
+                System.out.println(tv.getPrice());
+                System.out.println(tv.getScreenSize());
+            }
+        } catch (IOException e) {
+            System.out.println("ERROR: Problem encountered reading JSON file - " + e.getMessage());
+        }
+    }
+
+}
